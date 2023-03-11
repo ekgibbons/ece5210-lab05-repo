3a3fc141fc86696e4d9020fd0fe16a3b95432880
ekgibbons <ekgibbons@gmail.com>
2023-03-11 16:29:31 -0700

    Add files via upload

---
+++ Core/Src/ece5210.c
@@ -0,0 +1,14 @@
+#include "ece5210.h"
+
+#include <stdio.h>
+
+
+int16_t process_sample(int16_t sample_in)
+{
+    int16_t sample_out = 0;
+    float sample_in_f = (float)sample_in;
+    float sample_out_f = 0;
+
+    sample_out = (int16_t)sample_out_f;
+    return sample_out;
+}
